555d35b008a5a01dffd49eb55072b060311e35e9
Форов Александр <5h9t5@list.ru>
2024-09-26 14:56:19 +0300

    feat(models/wallet): change Float -> Decimal and Numeric

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Float, CheckConstraint
+from decimal import Decimal
+
+from sqlalchemy import CheckConstraint, Numeric
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.db import Base
 
 
 class Wallet(Base):
-    amount: Mapped[float] = mapped_column(Float, nullable=False, default=0)
+    amount: Mapped[Decimal] = mapped_column(
+        Numeric(precision=10, scale=2), nullable=False, default=Decimal('0.0')
+    )
 
     __table_args__ = (
-        CheckConstraint('amount > 0.0', name='check_amount_positive'),
+        CheckConstraint('amount >= 0.0', name='check_amount_positive'),
     )
